202bb03cd0cb8d4acd188bdc40ca8d64d3281939
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-10-13 16:47:32 +0300

    first commit

---
+++ scr/utils/getPercents.js
@@ -0,0 +1,11 @@
+const getPercents = (percent, number) => {
+	if(percent < 0 || percent > 100 || isNaN(percent) || typeof(percent) !== "number")  {
+		return "Первое значение должно быть числом больше 0 (невключительно) и меньше 100 (включительно).";
+	}
+	if(typeof(number) !== "number" || isNaN(number)) {
+		return "Второе значение должно быть числом.";
+	}
+	return number / 100 * percent;;
+}
+
+export default getPercents;
